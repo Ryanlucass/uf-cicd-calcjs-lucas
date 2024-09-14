d89b8143f221a936c4405dbfeff40bb74decd0df
lucasryanlandim <lucasryan@pmenos.com.br>
2024-09-14 13:16:19 -0300

    merge

--- calculadora.js
+++ calculadora.js
@@ -18,3 +18,32 @@ module.exports = {
     add,
     subtract
 };
+
+
+   // Função de multiplicação
+   function multiply(a, b) {
+    return a * b;
+}
+
+// Exportar as funções
+module.exports = {
+  add,
+  subtract,
+  multiply
+};
+
+
+// Função de divisão
+function divide(a, b) {
+    if (b === 0) {
+    throw new Error("Divisão por zero não é permitida.");
+    }
+    return a / b;
+}
+
+// Exportar as funções
+module.exports = {
+    add,
+    subtract,
+    divide,
+};

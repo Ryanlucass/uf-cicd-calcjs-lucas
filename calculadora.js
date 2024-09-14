8e2bf75a35f151abec67be91eaac49c3804a32a4
lucasryanlandim <lucasryan@pmenos.com.br>
2024-09-14 13:07:16 -0300

    criando a função subtração

---
+++ calculadora.js
@@ -0,0 +1,20 @@
+// Função de adição
+function add(a, b) {
+    return a + b;
+}   
+
+// Exportar as funções
+module.exports = {
+    add
+};    
+
+// Função de subtração
+function subtract(a, b) {
+    return a - b;
+}
+
+// Exportar as funções
+module.exports = {
+    add,
+    subtract
+};
